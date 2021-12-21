424c7fbaa5f99b37918c7489681a10cfe6e6c58f
Josh Johanning <joshjohanning@github.com>
2021-12-20 23:10:30 -0600

    moving backend.tf to separate file

--- app/main.tf
+++ app/main.tf
@@ -11,17 +11,6 @@ provider "azurerm" {
   features {}
 }
 
-# remote state
-# ===========================
-terraform {
-  backend "azurerm" {
-    subscription_id      = "93ddb8b3-ddb9-4d1c-ba59-421456e65538"
-    resource_group_name  = "rg-tailspin-tfstate"
-    storage_account_name = "tailspintfstate"
-    container_name       = "tfstate"
-  }
-}
-
 # create resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
