95a02daf33bde0427f33b6eca223ebacdc5182e3
Josh Johanning <joshjohanning@github.com>
2024-08-12 12:41:42 -0500

    feat: updating state terraform

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -13,24 +13,28 @@ terraform {
       version = ">= 3.115.0"
     }
   }
+
   backend "azurerm" {
-    resource_group_name   = "rg-tailspin-tfstate"
-    storage_account_name  = "tailspintfstate"
-    container_name        = "tfstate"
+    resource_group_name  = "rg-tailspin-tfstate"
+    storage_account_name = "tailspintfstate"
+    container_name       = "tfstate"
   }
 }
 
 resource "azurerm_resource_group" "tfstate" {
-  name = "rg-tailspin-tfstate"
+  name     = "rg-tailspin-tfstate"
   location = "centralus"
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tailspintfstate"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = azurerm_resource_group.tfstate.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  name                             = "tailspintfstate"
+  resource_group_name              = azurerm_resource_group.tfstate.name
+  location                         = azurerm_resource_group.tfstate.location
+  account_tier                     = "Standard"
+  account_replication_type         = "GRS"
+  min_tls_version                  = "TLS1_1"
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
 }
 
 resource "azurerm_storage_container" "tfstate" {
